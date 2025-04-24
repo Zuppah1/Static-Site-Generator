6c24375a206531be4d8a34eea3d989f8537af496
Zuppah1 <mantvydas.rinkunas@gmail.com>
2025-04-24 14:18:17 +0300

    BlockType, block_to_block_type function, unit tests

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,3 +1,14 @@
+import re
+from enum import Enum
+
+class BlockType(Enum):
+    PARAGRAPH= "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
 def markdown_to_blocks(markdown):
     markdown_list = []
     split_list = markdown.split("\n\n")
@@ -8,3 +19,25 @@ def markdown_to_blocks(markdown):
         markdown_list.append(block)
     return markdown_list
 
+
+
+def block_to_block_type(block):
+    if block.startswith("#") and " " in block and len(block.split(" ")[0]) <= 6:
+        return BlockType.HEADING
+    
+    elif block.startswith("```") and block.endswith("```"):
+        return BlockType.CODE
+    
+    elif all(line.startswith(">") for line in block.splitlines()):
+        return BlockType.QUOTE
+    
+    elif all(line.startswith("- ") for line in block.splitlines()):
+        return BlockType.UNORDERED_LIST
+    
+    elif all(line.split(". ")[0].isdigit() and int(line.split(". ")[0]) == i + 1 for i, line in enumerate(block.splitlines())):
+        return BlockType.ORDERED_LIST
+    
+    else:
+        return BlockType.PARAGRAPH
+
+       
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -1,5 +1,5 @@
 import unittest
-from block_markdown import markdown_to_blocks
+from block_markdown import markdown_to_blocks, block_to_block_type, BlockType
 
 class TestBlockMarkdown(unittest.TestCase):
     def test_markdown_to_blocks(self):
@@ -66,3 +66,71 @@ This is the same paragraph on a new line
                 "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line"
             ],
         )
+
+class TestBlockToBlockType(unittest.TestCase):
+    def test_block_to_block_type_paragraph(self):
+        string = "This is a regular paragraph"
+        result = BlockType.PARAGRAPH
+        block = block_to_block_type(string)
+        self.assertEqual(block, result)
+
+    def test_block_to_block_type_code(self):
+        string = "```This is a block of code```"
+        result = BlockType.CODE
+        block = block_to_block_type(string)
+        self.assertEqual(block, result)
+
+    def test_block_to_block_type_quote(self):
+        string = ">This is a" \
+        ">quote block" \
+        ">with several lines"
+        result = BlockType.QUOTE
+        block = block_to_block_type(string)
+        self.assertEqual(block, result)
+
+    def test_block_to_block_type_ordered(self):
+        string = "1. This" \
+        "2. is an ordered" \
+        "3. list"
+        result = BlockType.ORDERED_LIST
+        block = block_to_block_type(string)
+        self.assertEqual(block, result)
+
+    def test_block_to_block_type_unordered(self):
+        string = "- This" \
+        "- is an unordered" \
+        "- list"
+        result = BlockType.UNORDERED_LIST
+        block = block_to_block_type(string)
+        self.assertEqual(block, result)
+
+    def test_block_to_block_type_heading(self):
+        string = "#### This is a heading"
+        result = BlockType.HEADING
+        block = block_to_block_type(string)
+        self.assertEqual(block, result)
+    
+    def test_block_to_block_type_bad_syntax1(self):
+        string = "######### This is a bad heading, hence its a paragraph"
+        result = BlockType.PARAGRAPH
+        block = block_to_block_type(string)
+        self.assertEqual(block, result)
+
+    def test_block_to_block_type_bad_syntax2(self):
+        string = "```This is a bad code block, hence its a paragraph"
+        result = BlockType.PARAGRAPH
+        block = block_to_block_type(string)
+        self.assertEqual(block, result)
+
+    def test_block_to_block_type_bad_syntax3(self):
+        string = " >This is a bad quote block, \n>hence its a paragraph"
+        result = BlockType.PARAGRAPH
+        block = block_to_block_type(string)
+        self.assertEqual(block, result)
+
+    def test_block_to_block_type_bad_syntax4(self):
+        string = "1. This is a bad ordered list block,\n1. hence its a paragraph"
+        result = BlockType.PARAGRAPH
+        block = block_to_block_type(string)
+        self.assertEqual(block, result)
+    
